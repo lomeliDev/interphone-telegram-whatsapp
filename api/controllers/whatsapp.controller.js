aa0419247c8f0541800c7e49db7f50df14c92bbb
lomelidev <miguel@lomeli.io>
2021-09-14 18:00:01 -0500

    Enviamos el mensaje de bienvenida

--- api/controllers/whatsapp.controller.js
+++ api/controllers/whatsapp.controller.js
@@ -123,10 +123,28 @@ class WhatsappController {
         });
     }
 
+    welcome() {
+        try {
+            this.client.sendMessage(this._config.ADMIN_NUMBER_1 + "@c.us", "WhatsApp is listening");
+
+            if (this._config.ADMIN_NUMBER_1 !== this._config.ADMIN_NUMBER_2) {
+                this.client.sendMessage(this._config.ADMIN_NUMBER_2 + "@c.us", "WhatsApp is listening");
+            }
+
+            if (this._config.ADMIN_NUMBER_1 !== this._config.ADMIN_NUMBER_3 && this._config.ADMIN_NUMBER_2 !== this._config.ADMIN_NUMBER_3) {
+                this.client.sendMessage(this._config.ADMIN_NUMBER_3 + "@c.us", "WhatsApp is listening");
+            }
+        } catch (error) {
+            this._log.error(error.message);
+        }
+    }
+
     app() {
         try {
             if (this.auth) {
                 this._log.log("Ready to interact with whatsapp");
+                this.welcome();
+
             } else {
                 this._log.log("You're not online");
             }
