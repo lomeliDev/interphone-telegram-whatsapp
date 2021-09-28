5f591b56c1d98e0b12d6e3fbd1920844258ab633
lomelidev <miguel@lomeli.io>
2021-09-28 23:35:41 +0100

    Se agrego a la foto y video la opcion para la camara del esp32

--- api/controllers/photo.controller.js
+++ api/controllers/photo.controller.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const fs = require('fs');
+const shellExec = require('shell-exec');
+const fetch = require('node-fetch');
 
 class PhotoController {
 
@@ -23,21 +25,35 @@ class PhotoController {
                 fs.unlinkSync(this.pathPhoto);
             } catch (error) { }
             setTimeout(() => {
-                const result = this._stream.getLastFrame();
-                if (result !== null) {
-                    try {
-                        fs.writeFile(this.pathPhoto, result, (err) => {
-                            if (err) {
-                                callback(this.pathPhoto, arg_1, arg_2, false);
-                            } else {
-                                callback(this.pathPhoto, arg_1, arg_2, true);
-                            }
-                        })
-                    } catch (error) {
+                if (this._config.CAMERA === "ESP32") {
+                    fetch(`http://${this._config.HOST_CAMERA}/on`).then(res => res).then(json => { });
+                    const streamUrl = `http://${this._config.HOST_CAMERA}/mjpeg/1`;
+                    shellExec(`ffmpeg -f mjpeg -r 6 -ss 00:00:00 -i "${streamUrl}" -s 640x480 -vframes 1 -q:v 2 -y ${this.pathPhoto}`);
+                    setTimeout(() => {
+                        fetch(`http://${this._config.HOST_CAMERA}/off`).then(res => res).then(json => { });
+                        try {
+                            callback(this.pathPhoto, arg_1, arg_2, true);
+                        } catch (error) {
+                            callback(this.pathPhoto, arg_1, arg_2, false);
+                        }
+                    }, 13000);
+                } else {
+                    const result = this._stream.getLastFrame();
+                    if (result !== null) {
+                        try {
+                            fs.writeFile(this.pathPhoto, result, (err) => {
+                                if (err) {
+                                    callback(this.pathPhoto, arg_1, arg_2, false);
+                                } else {
+                                    callback(this.pathPhoto, arg_1, arg_2, true);
+                                }
+                            })
+                        } catch (error) {
+                            callback(this.pathPhoto, arg_1, arg_2, false);
+                        }
+                    } else {
                         callback(this.pathPhoto, arg_1, arg_2, false);
                     }
-                } else {
-                    callback(this.pathPhoto, arg_1, arg_2, false);
                 }
             }, 3000);
         }
--- api/controllers/video.controller.js
+++ api/controllers/video.controller.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 const shellExec = require('shell-exec');
+const fetch = require('node-fetch');
 
 class VideoController {
 
@@ -25,10 +26,22 @@ class VideoController {
             } catch (error) { }
             setTimeout(() => {
                 try {
-                    shellExec(`ffmpeg -f mjpeg -r 16 -i "http://127.0.0.1:${this._config.PORT_API}/stream/stream.mjpg" -b:v 4000k -c:v libx264 -crf 19 -r 8 -s 640x480 -to 00:00:15 -y ${this.pathVideo}`);
+                    let streamUrl = `http://127.0.0.1:${this._config.PORT_API}/stream/stream.mjpg`;
+                    let timeCallBack = 25000;
+                    if (this._config.CAMERA === "ESP32") {
+                        timeCallBack = 65000;
+                        streamUrl = `http://${this._config.HOST_CAMERA}/mjpeg/1`;
+                        fetch(`http://${this._config.HOST_CAMERA}/on`).then(res => res).then(json => { });
+                        shellExec(`ffmpeg -f mjpeg -r 6 -i "${streamUrl}" -b:v 4000k -c:v libx264 -crf 19 -r 6 -s 640x480 -to 00:00:15 -y ${this.pathVideo}`);
+                    } else {
+                        shellExec(`ffmpeg -f mjpeg -r 16 -i "${streamUrl}" -b:v 4000k -c:v libx264 -crf 19 -r 8 -s 640x480 -to 00:00:15 -y ${this.pathVideo}`);
+                    }
                     setTimeout(() => {
+                        if (this._config.CAMERA === "ESP32") {
+                            fetch(`http://${this._config.HOST_CAMERA}/off`).then(res => res).then(json => { });
+                        }
                         callback(this.pathVideo, arg_1, arg_2, true);
-                    }, 25000);
+                    }, timeCallBack);
                 } catch (e) {
                     callback(this.pathVideo, arg_1, arg_2, result);
                 }
